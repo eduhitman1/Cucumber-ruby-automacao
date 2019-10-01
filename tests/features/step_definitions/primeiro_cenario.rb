e54a863cf826ff235e90b4dddc9ae056d511c184
Eduardo Henrique <edu.hitman01.eh@gmail.com>
2019-10-01 13:30:18 -0300

    resultado de soma

--- tests/features/step_definitions/primeiro_cenario.rb
+++ tests/features/step_definitions/primeiro_cenario.rb
@@ -1,9 +1,7 @@
 Quando("eu somo {int} + {int}") do |valor1, valor2|
-    @Soma = valor1 + valor2
+    @soma = valor1 + valor2
   end
   
-  Então("o resultado tem que ser {int}") do |int|
+  Então("o resultado tem que ser {int}") do |resultado|
     expect(@soma).to eq resultado
-    puts @soma
-    puts resultado
   end
